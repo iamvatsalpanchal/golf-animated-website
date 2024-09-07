8f3381c144d624d28f3ec43470d58c3d9e49e16c
iamvatsalpanchal <vatsalpanchal4488@gmail.com>
2024-09-07 12:20:55 +0530

    adding page4, cards, and footer

--- script.js
+++ script.js
@@ -102,3 +102,15 @@ gsap.from("#about-us img,#about-us-in", {
       scrub: 4,
     },
   });
+
+  gsap.from("#page4 h1", {
+    y: 50,
+    scrollTrigger: {
+      trigger: "#page4 h1",
+      scroller: "body",
+      // markers:true,
+      start: "top 75%",
+      end: "top 70%",
+      scrub: 3,
+    },
+  });
